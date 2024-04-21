8c8806782fee92a0df2f37a70419cf8284ad35fb
hectorchu~ <hectorchu8@gmail.com>
2024-04-21 16:18:03 -0700

    Add __str__ to models - Add Portfolio field "name"

--- portfolios/models.py
+++ portfolios/models.py
@@ -5,10 +5,16 @@ from django.contrib.auth.models import User
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.user.name
+
 
 class Stock(models.Model):
     symbol = models.CharField(max_length=10)
     name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
 
 
 class Transaction(models.Model):
@@ -20,10 +26,17 @@ class Transaction(models.Model):
     type = models.CharField(max_length=10)
     timestamp = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.user} {self.stock} {self.quantity} {self.type}"
+
 
 class Portfolio(models.Model):
+    name = models.CharField(max_length=20)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     stocks = models.ManyToManyField(Stock, through='PortfolioStock')
+
+    def __str__(self):
+        return self.name
 
 
 class PortfolioStock(models.Model):
@@ -31,3 +44,6 @@ class PortfolioStock(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     # TODO: add validator to check that value >= 0
     quantity = models.DecimalField(max_digits=10, decimal_places=2)
+
+    def __str__(self):
+        return f"{self.portfolio} {self.stock} {self.quantity}"
