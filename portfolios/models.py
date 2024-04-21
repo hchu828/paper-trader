b70828b2cf55f35f7a1ef5a7ea42de9f02540232
hectorchu~ <hectorchu8@gmail.com>
2024-04-21 15:55:12 -0700

    Add Transaction field "type" for buy/sell/etc

--- portfolios/models.py
+++ portfolios/models.py
@@ -16,6 +16,7 @@ class Transaction(models.Model):
     stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
     quantity = models.DecimalField(min_value=0, decimal_places=3)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    type = models.CharField(max_length=10)
     timestamp = models.DateTimeField(auto_now_add=True)
 
 
