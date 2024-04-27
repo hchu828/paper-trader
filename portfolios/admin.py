56fa4ce6427e5752c37c4787794996f154bbcd0b
hectorchu~ <hectorchu8@gmail.com>
2024-04-27 15:23:40 -0700

    Create CustomUser and CustomAdmin - Create superuser - Add user forms

--- portfolios/admin.py
+++ portfolios/admin.py
@@ -1,9 +1,23 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-from .models import Portfolio, Stock, PortfolioStock, Transaction, CustomUser
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import Portfolio, Stock, PortfolioStock, Transaction
 
+CustomUser = get_user_model()
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+
+    list_display = ["email", "username", "is_superuser"]
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
 admin.site.register(Portfolio)
 admin.site.register(Stock)
 admin.site.register(PortfolioStock)
 admin.site.register(Transaction)
-admin.site.register(CustomUser)
