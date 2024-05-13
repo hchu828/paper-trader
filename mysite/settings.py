dbfa26f7e994a58232f31dba7448705a53d4eec0
hectorchu~ <hectorchu8@gmail.com>
2024-05-12 20:31:34 -0700

    Add Pages App with templates

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,13 +36,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    "portfolios.apps.PortfoliosConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "portfolios.apps.PortfoliosConfig",
+    "pages.apps.PagesConfig",
 ]
 
 MIDDLEWARE = [
@@ -60,7 +61,7 @@ ROOT_URLCONF = "mysite.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- mysite/urls.py
+++ mysite/urls.py
@@ -19,6 +19,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("portfolios/", include("portfolios.urls")),
     path("admin/", admin.site.urls),
+    path("portfolios/", include("portfolios.urls")),
+    path("", include("pages.urls")),
 ]
