8da8ce5e61c34b800c4eade278a828ccada21dd8
hectorchu~ <hectorchu8@gmail.com>
2024-05-14 18:07:52 -0700

    Implement login - Only applies to superuser

--- mysite/urls.py
+++ mysite/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     # Django admin
     path("admin/", admin.site.urls),
     # User management
-    path("accounts/", include("accounts.urls")),
+    path("accounts/", include("django.contrib.auth.urls")),
     # Local apps
     path("", include("pages.urls")),
 ]
