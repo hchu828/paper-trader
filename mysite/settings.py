aaf340fec1e8ee21099d95d526b6e5e9e1e2c72d
hectorchu~ <hectorchu8@gmail.com>
2024-05-25 20:53:13 -0700

    Make static file storage explicit - aggregates all static files into a single directory for production performance

--- mysite/settings.py
+++ mysite/settings.py
@@ -135,8 +135,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "theme/static/"
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+STATICFILES_DIRS = [BASE_DIR / "theme" / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
