c649c77cad64bd34b159cf5d0009c0ac7b6c03eb
hectorchu~ <hectorchu8@gmail.com>
2024-05-25 21:56:59 -0700

    Bugfix: create_superuser test missing 'test_' prefix

--- accounts/tests.py
+++ accounts/tests.py
@@ -20,18 +20,18 @@ class CustomUserTests(TestCase):
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
-    def create_superuser(self):
+    def test_create_superuser(self):
         User = get_user_model()
-        user = User.objects.create_superuser(
+        admin_user = User.objects.create_superuser(
             username="super_admin",
             email="superadmin@email.com",
             password="test_pass123",
         )
-        self.assertEqual(user.username, "super_admin")
-        self.assertEqual(user.email, "superadmin@email.com")
-        self.assertTrue(user.is_active)
-        self.assertTrue(user.is_staff)
-        self.assertTrue(user.is_superuser)
+        self.assertEqual(admin_user.username, "super_admin")
+        self.assertEqual(admin_user.email, "superadmin@email.com")
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
 
 
 class SignupPageTests(TestCase):
