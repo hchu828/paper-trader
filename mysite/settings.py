723b711d4d2011a1e5647b79c6635c98247f9709
hectorchu~ <hectorchu8@gmail.com>
2024-05-31 16:56:18 -0700

    Use environs instead of dotenv - Add .envrc to .gitignore - Move SECRET_KEY to docker-compose.yml
    Note: consider an alternative to .env later

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,22 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
-from dotenv import load_dotenv
+from environs import Env
 
-load_dotenv()
+env = Env()
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-i#tldv&k%bwl3+1z*6^k!6l_uy%wijvqp212ly7)zv19y7w(q*"
+SECRET_KEY = env.str("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -95,11 +94,11 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASSWORD"),
+        "NAME": env.str("DB_NAME"),
+        "USER": env.str("DB_USER"),
+        "PASSWORD": env.str("DB_PASSWORD"),
         "HOST": "db",
-        "PORT": os.environ.get("DB_PORT"),
+        "PORT": env.str("DB_PORT"),
     },
     "sqlite": {
         "ENGINE": "django.db.backends.sqlite3",
